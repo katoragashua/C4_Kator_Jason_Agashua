ba547688b0be291e2824cc0ecc148e13cf122af1
katoragashua <katoragashua@outlook.com>
2023-11-08 10:29:21 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+      <p>I am Kator Jason Agashua, this is my first React application.</p>
+    </main>
+  );
+}
+
+export default App
