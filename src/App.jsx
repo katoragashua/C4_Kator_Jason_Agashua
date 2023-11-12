de1a0f53f129ed04fb9ec22773017af88811f244
katoragashua <katoragashua@outlook.com>
2023-11-12 15:26:07 +0100

    My first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <main>
       <h1>Hello world</h1>
-      <p>I am Kator Jason Agashua, this is my first React application.</p>
+      <Portfolio />
     </main>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <main>
+      <h2>Name: Kator Jason Agashua</h2>
+      <div>
+        <h3>Education</h3>
+        <ul>
+          <li>Univerity of Agriculture, Makurdi, Nigeria.</li>
+          <li>Scrimba Advanced React Certificate</li>
+          <li>Node Js Tutorial and Projects Course (John Smilga), Udemy</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Professional Background</h3>
+        <li>Frontend Developer Intern: Lykluk International inc.</li>
+      </div>
+      <div>
+        <h3>Skills</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React JS</li>
+            <li>Node JS</li>
+            <li>Express</li>
+            <li>Mongo DB</li>
+            <li>Redux</li>
+            <li>Git</li>
+            <li>Redux</li>
+            <li>Tailwind</li>
+            <li>RESTful APIs</li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Portfolio;
