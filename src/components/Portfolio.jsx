15f0bd9762b0dbf84ebbdfb09e7dbe657b232cb8
katoragashua <katoragashua@outlook.com>
2023-11-12 17:00:43 +0100

    Refactored Portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Portfolio = () => {
   return (
     <main>
-      <h2>Name: Kator Jason Agashua</h2>
+      <div>
+        <h3>Name</h3>
+        <h2>Kator Jason Agashua</h2>
+      </div>
       <div>
         <h3>Education</h3>
         <ul>
@@ -19,18 +22,18 @@ const Portfolio = () => {
       <div>
         <h3>Skills</h3>
         <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React JS</li>
-            <li>Node JS</li>
-            <li>Express</li>
-            <li>Mongo DB</li>
-            <li>Redux</li>
-            <li>Git</li>
-            <li>Redux</li>
-            <li>Tailwind</li>
-            <li>RESTful APIs</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React JS</li>
+          <li>Node JS</li>
+          <li>Express</li>
+          <li>Mongo DB</li>
+          <li>Redux</li>
+          <li>Git</li>
+          <li>Redux</li>
+          <li>Tailwind</li>
+          <li>RESTful APIs</li>
         </ul>
       </div>
     </main>
